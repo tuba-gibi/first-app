bbf8f646ce447a7769ec5d3cd6bd239cae7bae33
tuba-gibi <eetede84@gmail.com>
2023-07-08 08:29:44 +0300

    css started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/style.css'
 
 import "bootstrap/dist/css/bootstrap.css";
 import Homepage from './pages/Homepage';
--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,11 +1,12 @@
 const Pictures = ({ name, img}) => {
     console.log(name)
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={img} className="card-img-top" alt="..." />
+    <div className="card m-4 p-3 rounded-4" style={{ width: "18rem" ,  height: "30rem" }}>
+      <div className="img-container h-100">
+        <img src={img} className="card-img-top" alt="image" />
+      </div>
       <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-     
+        <h5 className="text-success card-title">{name}</h5>
       </div>
     </div>
   );
